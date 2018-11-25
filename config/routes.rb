10a0d0b369e85a8c57e8729d31e8f41b0f698363
Suttikreat Pienwittayaskul <h3ngs0ft@gmail.com>
2018-11-25 21:18:31 +0700

    Customized routes portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios,except: [:show]
+  get 'portfolios/:id', to: 'portfolios#show' , as: 'portfolio_show'
   #get 'pages/home'
   #get 'pages/about'
   #get 'pages/contact'
