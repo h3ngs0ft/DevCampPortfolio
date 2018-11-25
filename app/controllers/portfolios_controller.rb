ae78256d4165d1a75e168775e9faf052eb7a0784
Suttikreat Pienwittayaskul <h3ngs0ft@gmail.com>
2018-11-25 20:55:34 +0700

    Implemented delete button for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,4 +32,16 @@ class PortfoliosController < ApplicationController
 	def show
 		@portfolio_item = Portfolio.find(params[:id])
 	end
+	def destroy
+		#Perform the lookup
+		@portfolio_item = Portfolio.find(params[:id])
+
+		#Destroy/Delete the record
+		@portfolio_item.destroy
+
+		#Redirect
+		respond_to do |format|
+		  format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+		end
+	end
 end
